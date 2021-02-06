cdc6858bb0cc4f22c988f6c8bbfbf6bd75145466
abarnez <alexander_barnes@outlook.com>
2021-02-05 20:29:42 -0500

    Added Options and Button Script
    can go to options menu and back to start menu via the buttons

---
+++ TowerDefence/Assets/_Script/ButtonScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Button _startButton, _loadButton, _optionsButton, _exitButton, _backButton;
+    public Canvas _startCanvas, _optionsCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _optionsButton.onClick.AddListener(TaskOnClickOp); 
+        _backButton.onClick.AddListener(TaskOnClickBa);
+        _optionsCanvas.GetComponent<Canvas>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClickOp()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        _startCanvas.GetComponent<Canvas>().enabled = false;
+        _optionsCanvas.GetComponent<Canvas>().enabled = true;
+    } 
+    void TaskOnClickBa()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        _startCanvas.GetComponent<Canvas>().enabled = true;
+        _optionsCanvas.GetComponent<Canvas>().enabled = false;
+    }
+}
