a8d077b704e73d6174b87d6bb93e6f574cb2ed85
JMakim <jmakimramjahn@gmail.com>
2021-02-21 22:06:41 -0500

    quick fix

--- Assets/_Script/audioScript.cs
+++ Assets/_Script/audioScript.cs
@@ -18,7 +18,10 @@ public class audioScript : MonoBehaviour
     {
         menuScene = audio[0];
 
+        
         cannonTurret = audio[3];
+        bombTurretFiring = audio[4];
+        bombTurretExplosion = audio[5];
     }
 
     // Update is called once per frame
