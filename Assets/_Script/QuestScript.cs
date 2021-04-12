9bbf66dc11846d80218f1ea6ae8f29b3f7ff7948
abarnez <alexander_barnes@outlook.com>
2021-04-11 21:54:00 -0400

    quest system

---
+++ Assets/_Script/QuestScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestScript : MonoBehaviour
+{
+    public Button questButton, xButton;
+    public Canvas questCanvas;
+    public TMP_Text Quest1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        questCanvas.gameObject.SetActive(false);
+        questButton.onClick.AddListener(TaskOnClick);
+        xButton.onClick.AddListener(TaskOnClick1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerPrefs.GetInt("FirstTower") == 1)
+        {
+            Quest1.text = "Place a Tower (1/1) - Complete";
+        }
+    }
+
+    void TaskOnClick()
+    {
+        questCanvas.gameObject.SetActive(true);
+    }
+
+    void TaskOnClick1()
+    {
+        questCanvas.gameObject.SetActive(false);
+    }
+}
